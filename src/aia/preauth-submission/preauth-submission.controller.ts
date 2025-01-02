782cc0c92df3a65bc80903ab5bf356661321f2e5
hchdevv1 <hchdev.v1@gmail.com>
2025-01-02 09:58:05 +0700

    set preauth-submission

--- src/aia/preauth-submission/preauth-submission.controller.ts
+++ src/aia/preauth-submission/preauth-submission.controller.ts
@@ -1,34 +1,18 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { PreauthSubmissionService } from './preauth-submission.service';
-import { CreatePreauthSubmissionDto } from './dto/create-preauth-submission.dto';
-import { UpdatePreauthSubmissionDto } from './dto/update-preauth-submission.dto';
+
 
 @Controller('preauth-submission')
 export class PreauthSubmissionController {
   constructor(private readonly preauthSubmissionService: PreauthSubmissionService) {}
 
-  @Post()
-  create(@Body() createPreauthSubmissionDto: CreatePreauthSubmissionDto) {
-    return this.preauthSubmissionService.create(createPreauthSubmissionDto);
-  }
+ 
 
   @Get()
   findAll() {
-    return this.preauthSubmissionService.findAll();
+    return 'preauthSubmissionService';
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.preauthSubmissionService.findOne(+id);
-  }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updatePreauthSubmissionDto: UpdatePreauthSubmissionDto) {
-    return this.preauthSubmissionService.update(+id, updatePreauthSubmissionDto);
-  }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.preauthSubmissionService.remove(+id);
-  }
 }
